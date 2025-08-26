2696dfe7a3a73fa906112ffc0f69a206c9582c1d
Shannon Craven <shannon@drake.so>
2025-08-26 17:47:38 -0400

    added OpenAI API key

--- config/env/development.js
+++ config/env/development.js
@@ -11,5 +11,5 @@ module.exports = {
       `<script>document.write("<script src='http://" + (location.host || "localhost").split(":")[0] + ":35729/livereload.js'></" + "script>");</script>`
       // jshint +W101
    ],
-  //DEMO:2MS:OPEN_AI_SECRET
+  someApiKey: "1doo5nt182ccp1saqplab0nd9v",
 };
